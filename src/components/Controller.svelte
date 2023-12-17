43e3f153eb4564415e954e35f12b727e4732b51c
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-12-17 23:01:52 +0700

    feat: can play a message and clear interval

--- src/components/Controller.svelte
+++ src/components/Controller.svelte
@@ -2,38 +2,72 @@
   // imports
   import { cn } from '../lib/cn'
   import { store } from '../lib/store'
+  import type { Message } from '../types/Mesasge'
+
+  const MESSAGE_INTERVAL = 1000
 
   let messageDisplayIndex: number = 0
+  let messages: Message[] = []
 
   let className: string = ''
+  let playingInterval: number = 0
 
-  let classList = cn('bg-white p-4 rounded-md', className)
   export { className as class }
 
   const increment = () => {
+    if (messageDisplayIndex >= messages.length - 1) return
     store.update(({ messageDisplayIndex, ...rest }) => ({
       messageDisplayIndex: messageDisplayIndex + 1,
       ...rest,
     }))
   }
   const decrement = () => {
+    if (messageDisplayIndex <= -1) return // can show nothing
     store.update(({ messageDisplayIndex, ...rest }) => ({
       messageDisplayIndex: messageDisplayIndex - 1,
       ...rest,
     }))
   }
 
+  // when reach the end, stop playing
+  $: if (messageDisplayIndex === messages.length - 1) {
+    console.log('clear interval')
+    clearInterval(playingInterval)
+  }
+
+  const handlePlayPause = () => {
+    if (playingInterval !== 0) {
+      clearInterval(playingInterval)
+      playingInterval = 0
+      return
+    } else {
+      playingInterval = setInterval(() => {
+        increment()
+      }, MESSAGE_INTERVAL)
+    }
+  }
+
   // effects
   store.subscribe((value) => {
     messageDisplayIndex = value.messageDisplayIndex
+    messages = value.messages
   })
+
+  let classList = cn('bg-white p-4 rounded-md flex justify-between items-center', className)
 </script>
 
 <main class={classList}>
-  controller
+  <div class="border-[1px] border-neutral-300 rounded-md px-4 py-2 w-fit">
+    <span class="text-xl font-bold">{messageDisplayIndex + 1}</span> /
+    <span class="text-xl font-bold">{messages.length}</span>
+  </div>
 
-  <p>Current Message: {messageDisplayIndex}</p>
-
-  <button on:click={increment}>+ 1</button>
-  <button on:click={decrement}>- 1</button>
+  <div class="flex gap-2 mt-4">
+    <button on:click={increment} class="bg-neutral-700 text-white rounded-md px-4 py-2">+ 1</button>
+    <button on:click={decrement} class="bg-neutral-700 text-white rounded-md px-4 py-2">- 1</button>
+    <button on:click={handlePlayPause} class="bg-neutral-700 text-white rounded-md px-4 py-2"
+      >{playingInterval ? 'Pause' : 'Play'}</button
+    >
+    {playingInterval}
+  </div>
 </main>
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,32 +1,62 @@
-import { writable, type Writable } from "svelte/store";
-import type { Message } from "../types/Mesasge";
+import { writable, type Writable } from 'svelte/store'
+import type { Message } from '../types/Mesasge'
 
 interface Store {
-  messages: Message[];
-  messageDisplayIndex: number;
+  messages: Message[]
+  messageDisplayIndex: number
   // theme: "discord" | "line"
 }
 
 const intialStore: Store = {
   messages: [
     {
-      id: "1",
-      content: "foo",
-      avatarUrl: "https://avatars.githubusercontent.com/u/499270?v=4",
+      id: '1',
+      content: 'foo',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
     },
     {
-      id: "3",
-      content: "baz",
-      avatarUrl: "https://avatars.githubusercontent.com/u/499270?v=4",
+      id: '3',
+      content: 'baz',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
     },
     {
-      id: "2",
-      content: "bar",
-      avatarUrl: "https://avatars.githubusercontent.com/u/499270?v=4",
+      id: '2',
+      content: 'bar',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
+    },
+    {
+      id: '4',
+      content: 'foo',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
+    },
+    {
+      id: '5',
+      content: 'baz',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
+    },
+    {
+      id: '6',
+      content: 'bar',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
+    },
+    {
+      id: '7',
+      content: 'foo',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
+    },
+    {
+      id: '8',
+      content: 'baz',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
+    },
+    {
+      id: '9',
+      content: 'bar',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/499270?v=4',
     },
   ],
   messageDisplayIndex: 0,
-};
+}
 
 // export const theme = writable("light");
-export const store: Writable<Store> = writable(intialStore);
+export const store: Writable<Store> = writable(intialStore)
