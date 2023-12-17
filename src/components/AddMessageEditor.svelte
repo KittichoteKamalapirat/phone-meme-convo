e795ad49f090e20024dfb0d7d5b8a526bf433833
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-12-17 22:17:49 +0700

    feat: add addMessage editor

---
+++ src/components/AddMessageEditor.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { store } from '../lib/store'
+
+  // state
+  let content: string = ''
+  let avatarUrl: string = ''
+
+  //   handlers
+  const handleContent = (e: any) => {
+    // TODO fix type
+    content = e.target.value
+  }
+  const handleAvatarUrl = (e: any) => {
+    avatarUrl = e.target.value
+  }
+  const handleAddMessage = (e: any) => {
+    avatarUrl = e.target.value
+    store.update(({ messages, ...rest }) => ({
+      messages: [...messages, { id: String(Date.now()), content, avatarUrl }],
+      ...rest,
+    }))
+
+    content = ''
+    avatarUrl = ''
+  }
+</script>
+
+<!-- Add new message input -->
+<div class="border-[1px] border-neutral-200 rounded-md p-4 mt-4">
+  <div class="flex flex-col">
+    <label for="content">Content</label>
+    <input
+      type="text"
+      on:input={handleContent}
+      value={content}
+      class="border-[1px] rounded-md px-2 py-1"
+    />
+  </div>
+
+  <div class="flex flex-col">
+    <label for="content">Avatar Image Url</label>
+    <input
+      type="text"
+      on:input={handleAvatarUrl}
+      value={avatarUrl}
+      class="border-[1px] rounded-md px-2 py-1"
+    />
+  </div>
+
+  <button class="bg-teal-500 text-white rounded-md px-4 py-1 mt-2" on:click={handleAddMessage}
+    >Add</button
+  >
+</div>
--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,82 +1,14 @@
 <script lang="ts">
   import { cn } from '../lib/cn'
-  import { store } from '../lib/store'
-  import type { Message } from '../types/Mesasge'
+  import AddMessageEditor from './AddMessageEditor.svelte'
+  import MessageListEditor from './MessageListEditor.svelte'
 
   let className: string = ''
   export { className as class }
-  let classList = cn('bg-white p-4 rounded-md', className)
-
-  let messages: Message[] = []
-
-  const handleRemovemessage = (id: string) => {
-    const newMessages = messages.filter((message) => message.id !== id)
-    console.log('remove')
-    store.update(({ messages, ...rest }) => ({ messages: newMessages, ...rest }))
-  }
-
-  const moveUp = (id: string) => {
-    const newMessages = [...messages]
-
-    const index = newMessages.findIndex((message) => message.id === id)
-    if (index === 0) return
-    const message = messages[index]
-    newMessages[index] = messages[index - 1]
-    newMessages[index - 1] = message
-    store.update(({ messages, ...rest }) => ({ messages: newMessages, ...rest }))
-  }
-
-  const moveDown = (id: string) => {
-    const newMessages = [...messages]
-
-    const index = messages.findIndex((message) => message.id === id)
-    if (index === messages.length - 1) return
-    const message = messages[index]
-    newMessages[index] = messages[index + 1]
-    newMessages[index + 1] = message
-    store.update(({ messages, ...rest }) => ({ messages: newMessages, ...rest }))
-  }
-
-  // effects
-  store.subscribe((value) => {
-    messages = value.messages
-  })
+  let classList = cn('flex flex-col grow', className)
 </script>
 
 <main class={classList}>
-  <div class="flex flex-col gap-2">
-    {#each messages as message (message.id)}
-      <div class="">
-        <div class="flex items-center gap-4">
-          <div class="flex w-full">
-            <img
-              class="w-10 h-10 rounded-full mr-4"
-              src={message.avatarUrl}
-              alt="Avatar of Jonathan Reinink"
-            />
-            <div class="text-gray-900 w-full rounded-md bg-neutral-200 px-2 py-1">
-              <span class="align-middle text-gray-900">{message.content}</span>
-            </div>
-          </div>
-
-          <div class="flex gap-2">
-            <button
-              class="bg-red-500 rounded-md text-white px-2 py-1 hover:bg-red-400"
-              on:click={() => handleRemovemessage(message.id)}>Delete</button
-            >
-            <button
-              class="bg-white rounded-md border-neutral-500 border-[1px] px-2 py-1 hover:bg-neutral-100"
-              on:click={() => moveUp(message.id)}>Up</button
-            >
-            <button
-              class="bg-white rounded-md border-neutral-500 border-[1px] px-2 py-1 hover:bg-neutral-100"
-              on:click={() => moveDown(message.id)}>Down</button
-            >
-          </div>
-        </div>
-      </div>
-    {:else}
-      <p>Please add a conversation</p>
-    {/each}
-  </div>
+  <MessageListEditor class="grow" />
+  <AddMessageEditor />
 </main>
